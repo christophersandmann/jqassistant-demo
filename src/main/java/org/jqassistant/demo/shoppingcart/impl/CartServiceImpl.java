1d6045ca8341e43d6ce0c0f8e6632bf84778b898
Dirk Mahler <dirk.mahler@buschmais.com>
2014-12-05 11:13:29 +0100

    clean up for demo

--- src/main/java/org/jqassistant/demo/shoppingcart/impl/CartServiceImpl.java
+++ src/main/java/org/jqassistant/demo/shoppingcart/impl/CartServiceImpl.java
@@ -1,7 +1,13 @@
 package org.jqassistant.demo.shoppingcart.impl;
 
+import org.jqassistant.demo.dataimport.ImportService;
 import org.jqassistant.demo.shoppingcart.api.CartService;
 
+import javax.inject.Inject;
+
 public class CartServiceImpl implements CartService {
+
+//    @Inject
+//    private ImportService importService;
 
 }
---
+++ src/test/java/org/jqassistant/demo/usermanagement/test/ExampleTest.java
@@ -0,0 +1,13 @@
+package org.jqassistant.demo.usermanagement.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void test() {
+        assertTrue("Must be true", true);
+    }
+}
