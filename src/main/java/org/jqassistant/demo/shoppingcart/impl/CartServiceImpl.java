200468ebd63ddf30921fe476d9817e30abb30315
Dirk Mahler <dirk.mahler@buschmais.com>
2015-02-25 07:46:56 +0100

    prepare for 1.0.0-RC

--- src/main/java/org/jqassistant/demo/shoppingcart/impl/CartServiceImpl.java
+++ src/main/java/org/jqassistant/demo/shoppingcart/impl/CartServiceImpl.java
@@ -2,12 +2,13 @@ package org.jqassistant.demo.shoppingcart.impl;
 
 import org.jqassistant.demo.dataimport.ImportService;
 import org.jqassistant.demo.shoppingcart.api.CartService;
+import org.jqassistant.demo.usermanagement.api.CustomerService;
 
 import javax.inject.Inject;
 
 public class CartServiceImpl implements CartService {
 
-//    @Inject
-//    private ImportService importService;
+  //  @Inject
+  //  private ImportService importService;
 
 }
