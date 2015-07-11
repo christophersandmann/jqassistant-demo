559700ae0aeb69caa20b2873d26f09742d259d3b
Dirk Mahler <dirk.mahler@buschmais.com>
2015-07-11 17:30:58 +0200

    specify naming rules using asciidoc

--- src/main/java/org/jqassistant/demo/Module.java
+++ src/main/java/org/jqassistant/demo/Module.java
@@ -1,9 +1,22 @@
 package org.jqassistant.demo;
 
+/**
+ * Marks a package as a module
+ */
 public @interface Module {
 
+    /**
+     * The name of the module.
+     * 
+     * @return The name of the module.
+     */
     String value();
 
+    /**
+     * The names of modules this module depends on.
+     * 
+     * @return The names of modules this module depends on.
+     */
     String[] dependsOn() default {};
 
 }
--- src/main/java/org/jqassistant/demo/shoppingcart/impl/CartServiceImpl.java
+++ src/main/java/org/jqassistant/demo/shoppingcart/impl/CartServiceImpl.java
@@ -1,10 +1,13 @@
 package org.jqassistant.demo.shoppingcart.impl;
 
+import org.jqassistant.demo.dataimport.api.ImportService;
 import org.jqassistant.demo.shoppingcart.api.CartService;
+
+import javax.inject.Inject;
 
 public class CartServiceImpl implements CartService {
 
-  //  @Inject
-  //  private ImportService importService;
+//    @Inject
+//    private ImportService importService;
 
 }
